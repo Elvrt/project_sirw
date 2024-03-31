0fc44a42b2a22dbe8d1c3984fbf2f7fd24980821
Athdjani0503 <athallah.kulyah@gmail.com>
2024-04-01 00:23:02 +0700

    Update dashboard.blade.php

--- resources/views/Warga/dashboard.blade.php
+++ resources/views/Warga/dashboard.blade.php
@@ -63,7 +63,7 @@
           <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" stroke="currentColor" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16"></path>
           </svg>
-        </span>www
+        </span>
       </button>
     </div>
   </nav>
