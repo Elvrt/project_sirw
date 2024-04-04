01560640905e7408bddb454d43300b2b6d0d2372
Elvrt <elvarettas23@gmail.com>
2024-04-04 12:58:48 +0700

    tambah warna

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,6 +30,10 @@ export default {
       'linear1' : '#F0ECE9',
       'linear2' : '#FEF4E9',
       'backgroundform' : '#DFDFDF',
+      'kuning-gelap' : '#C8A116',
+      'merah-gelap' : '#BA1717',
+      'abu-gelap' : '#615B5B',
+      'hijau-gelap' : '#1D8104'
     },
     fontFamily: {
 
