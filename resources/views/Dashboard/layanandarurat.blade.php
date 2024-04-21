fedcd73358dcd5246be18f45bf82753f94de53e3
Athdjani0503 <athallah.kulyah@gmail.com>
2024-04-21 11:40:35 +0700

    WARGA MINUS MODAL

---
+++ resources/views/Dashboard/layanandarurat.blade.php
@@ -0,0 +1,70 @@
+@extends('./layout/warga')
+
+@section('content')
+<p class="text-center text-army-gelap font-bold text-header drop-shadow-md mt-10 mb-10">LAYANAN DARURAT</p>
+
+<div class="grid grid-cols-3 mx-20 px-20 py-20 bg-login2 ">
+    <div class=" grid grid-cols-2 gap-0 mx-6 my-6 items-center">
+        <div class="ml-20">
+            <img src="https://media.discordapp.net/attachments/1222905897848934450/1231455453079277650/image.png?ex=66370543&is=66249043&hm=bdb3c422f268e3bd720136edebd511d43dd00ad5676899de7e4e2509ba40eeae&=&format=webp&quality=lossless" alt="layanan" width="100">
+        </div>
+        <div class="text-left">
+            <p>Deskripsi Layanan</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+        </div>
+    </div>
+
+    <div class=" grid grid-cols-2 gap-0 mx-6 my-6 items-center">
+        <div class="ml-20">
+            <img src="https://media.discordapp.net/attachments/1222905897848934450/1231455453079277650/image.png?ex=66370543&is=66249043&hm=bdb3c422f268e3bd720136edebd511d43dd00ad5676899de7e4e2509ba40eeae&=&format=webp&quality=lossless" alt="layanan" width="100">
+        </div>
+        <div class="text-left">
+            <p>Deskripsi Layanan</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+        </div>
+    </div>
+
+    <div class=" grid grid-cols-2 gap-0 mx-6 my-6 items-center">
+        <div class="ml-20">
+            <img src="https://media.discordapp.net/attachments/1222905897848934450/1231455453079277650/image.png?ex=66370543&is=66249043&hm=bdb3c422f268e3bd720136edebd511d43dd00ad5676899de7e4e2509ba40eeae&=&format=webp&quality=lossless" alt="layanan" width="100">
+        </div>
+        <div class="text-left">
+            <p>Deskripsi Layanan</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+        </div>
+    </div>
+
+    <div class=" grid grid-cols-2 gap-0 mx-6 my-6 items-center">
+        <div class="ml-20">
+            <img src="https://media.discordapp.net/attachments/1222905897848934450/1231455453079277650/image.png?ex=66370543&is=66249043&hm=bdb3c422f268e3bd720136edebd511d43dd00ad5676899de7e4e2509ba40eeae&=&format=webp&quality=lossless" alt="layanan" width="100">
+        </div>
+        <div class="text-left">
+            <p>Deskripsi Layanan</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+        </div>
+    </div>
+
+    <div class=" grid grid-cols-2 gap-0 mx-6 my-6 items-center">
+        <div class="ml-20">
+            <img src="https://media.discordapp.net/attachments/1222905897848934450/1231455453079277650/image.png?ex=66370543&is=66249043&hm=bdb3c422f268e3bd720136edebd511d43dd00ad5676899de7e4e2509ba40eeae&=&format=webp&quality=lossless" alt="layanan" width="100">
+        </div>
+        <div class="text-left">
+            <p>Deskripsi Layanan</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+        </div>
+    </div>
+
+    <div class=" grid grid-cols-2 gap-0 mx-6 my-6 items-center">
+        <div class="ml-20">
+            <img src="https://media.discordapp.net/attachments/1222905897848934450/1231455453079277650/image.png?ex=66370543&is=66249043&hm=bdb3c422f268e3bd720136edebd511d43dd00ad5676899de7e4e2509ba40eeae&=&format=webp&quality=lossless" alt="layanan" width="100">
+        </div>
+        <div class="text-left">
+            <p>Deskripsi Layanan</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+        </div>
+    </div>
+
+</div>
+
+</div>
+@endsection
---
+++ resources/views/Warga/pengaduan.blade.php
@@ -0,0 +1,53 @@
+@extends('./layout/warga')
+
+@section('content')
+<!-- CONTENT -->
+<p class="text-center text-army-gelap font-bold text-header drop-shadow-md mt-10 mb-10">FORM PENGADUAN</p>
+<div class="bg-putih shadow-md mx-5 mb-2 px-3">
+    <p class="text-sub">Form Pengaduan</p>
+</div>
+
+<div class="bg-putih shadow-md mx-5 py-10 px-8">
+    <div class="mb-10">
+        <p>NIK</p>
+        <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+            <input type="tel" name="username" id="NIK" autocomplete="NIK" minlength="16" maxlength="16" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="NIK" required>
+        </div>
+    </div>
+    <div class="mb-10">
+        <p>Nama</p>
+        <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+            <input type="text" name="Nama" id="Nama" autocomplete="Nama" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Nama" required>
+        </div>
+    </div>
+    <div class="mb-10">
+        <p>Alamat</p>
+        <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+            <input type="text" name="Alamat" id="Alamat" autocomplete="Alamat" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Alamat" required>
+        </div>
+    </div>
+    <div class="mb-10">
+        <p>No. Telpon</p>
+        <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+            <input type="tel" name="NoTelpon" id="NoTelpon" autocomplete="NoTelpon" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="No.Telpon" required>
+        </div>
+    </div>
+    <div class="mb-10">
+        <p>RT</p>
+        <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+            <input type="number" name="NoTelpon" id="NoTelpon" autocomplete="NoTelpon" min="1" max="8" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="RT" required>
+        </div>
+    </div>
+    <div class="mb-10">
+        <p>Judul Pengaduan</p>
+        <textarea id="message" rows="1" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg  border border-army-gelap focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Judul Pengaduan"></textarea>
+    </div>
+    <div class="mb-10">
+        <p>Keterangan</p>
+        <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg  border border-army-gelap focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Keterangan"></textarea>
+    </div>
+
+    <div>
+        <button class=" w-max px-10 bg-coklat-muda text-abu-putih font-semibold py-2 rounded-xl mx-auto block hover:bg-coklat focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500 mt-10 mb-6">Kirim</button>
+    </div>
+    @endsection
