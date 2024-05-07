42e5581e41ed7d09e4349bb2c41a701f9980849e
Elvrt <elvarettas23@gmail.com>
2024-05-07 14:04:08 +0700

    sign out dashboard

--- resources/views/layout/rw.blade.php
+++ resources/views/layout/rw.blade.php
@@ -119,7 +119,7 @@
                     <svg class="w-5 h-5  text-putih transition duration-75 dark:text-gray-400 group-hover:text-army dark:group-hover:text-army" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                         <path fill-rule="evenodd" d="M16.5 3.75a1.5 1.5 0 0 1 1.5 1.5v13.5a1.5 1.5 0 0 1-1.5 1.5h-6a1.5 1.5 0 0 1-1.5-1.5V15a.75.75 0 0 0-1.5 0v3.75a3 3 0 0 0 3 3h6a3 3 0 0 0 3-3V5.25a3 3 0 0 0-3-3h-6a3 3 0 0 0-3 3V9A.75.75 0 1 0 9 9V5.25a1.5 1.5 0 0 1 1.5-1.5h6Zm-5.03 4.72a.75.75 0 0 0 0 1.06l1.72 1.72H2.25a.75.75 0 0 0 0 1.5h10.94l-1.72 1.72a.75.75 0 1 0 1.06 1.06l3-3a.75.75 0 0 0 0-1.06l-3-3a.75.75 0 0 0-1.06 0Z" clip-rule="evenodd" />
                     </svg>
-                    <span class=" text-putih flex-1 ms-3 whitespace-nowrap group-hover:text-army">Sign In</span>
+                    <span class=" text-putih flex-1 ms-3 whitespace-nowrap group-hover:text-army">Sign Out</span>
                 </a>
             </li>
         </ul>
