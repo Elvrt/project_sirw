15766ff5412b6725e041065aef88b739400ac7a3
Nafi'ul Alam Dary Vega <120392921+NafiulAlamDaryVega@users.noreply.github.com>
2024-05-11 16:04:22 +0700

    add crud kk, modify struktur rw

--- resources/views/RW/KartuKeluarga/index.blade.php
+++ resources/views/RW/KartuKeluarga/index.blade.php
@@ -5,20 +5,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Struktur RW</title>
+    <title>Kartu Keluarga</title>
     {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css"> --}}
 </head>
 <body>
 <div class="flex justify-center datas-center">
     <div class="p-4 sm:ml-64">
-        <p class="text-army-gelap font-bold text-header drop-shadow-md container mb-10 mt-10 ml-4">Data Struktur RW</p>
+        <p class="text-army-gelap font-bold text-header drop-shadow-md container mb-10 mt-10 ml-4">Data Kartu Keluarga</p>
         <div class="bg-putih drop-shadow-md mx-4 px-10 p-4">
-            {{-- <a href="StrukturRw/create">
+            <a href="KartuKeluarga/create">
                 <button class="bg-hijau hover:bg-hijau-gelap text-putih font-bold py-2 px-4 rounded-lg float-right">
-                    + Tambah Data Struktur RW
+                    + Tambah Data Kartu Keluarga
                 </button>
-            </a> --}}
+            </a>
             <p class="text-sub ml-4">Data</p>
         </div>
         <div class="flex justify-center datas-center">
@@ -64,14 +64,13 @@
                 <div class="col-span-6 mt-4 p-10 sm:ml-68 drop-shadow-md text-left mr-9">
                     <!-- HEADER -->
                     <div class="table-responsive">
-                        <table id="table_strukturRw" class="table-auto">
+                        <table id="table_kartuKeluarga" class="table-auto">
                             <thead>
                                 <tr>
                                     <th class="px-4 py-2">No.</th>
-                                    <th class="px-4 py-2">Kode</th>
-                                    <th class="px-4 py-2">Nama Struktur</th>
-                                    <th class="px-4 py-2">Nama Warga</th>
-                                    <th class="px-4 py-2">Nomor Telepon</th>
+                                    <th class="px-4 py-2">RT</th>
+                                    <th class="px-4 py-2">No. KK</th>
+                                    <th class="px-4 py-2">Kepala Keluarga</th>
                                     <th class="px-4 py-2">Action</th>
                                 </tr>
                             </thead>
@@ -79,25 +78,31 @@
                                 @foreach ($data as $data)
                                 <tr>
                                     <td class="px-4 py-2">{{$loop->iteration}}</td>
-                                    <td class="px-4 py-2">{{$data->kode_struktur}}</td>
-                                    <td class="px-4 py-2">{{$data->nama_struktur}}</td>
-                                    <td class="px-4 py-2">{{$data->warga->nama_warga}}</td>
-                                    <td class="px-4 py-2">{{$data->warga->nomor_telepon}}</td>
+                                    <td class="px-4 py-2">{{$data->rt->nomor_rt}}</td>
+                                    <td class="px-4 py-2">{{$data->no_kk}}</td>
+                                    <td class="px-4 py-2">
+                                        @php
+                                            $kepalaKeluarga = $data->warga->firstWhere('status_hubungan', 'Kepala Keluarga');
+                                            if ($kepalaKeluarga) {
+                                                echo $kepalaKeluarga->nama_warga;
+                                            }
+                                        @endphp
+                                    </td>
                                     <td class="px-4 py-2">
                                         <div class="flex gap-3">
-                                            <a href="/RW/StrukturRw/{{ $data->id_struktur }}/edit" class="bg-kuning hover:bg-kuning-gelap text-putih font-medium py-2 px-4 rounded-lg">
+                                            <a href="/RW/KartuKeluarga/{{ $data->id_kk }}/edit" class="bg-kuning hover:bg-kuning-gelap text-putih font-medium py-2 px-4 rounded-lg">
                                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
                                                     <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z" />
                                                     <path d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z" />
                                                 </svg>
                                             </a>
-                                            <a href="#" data-toggle="modal" data-target="#ModalShow{{ $data->id_struktur }}" class="detail bg-abu-tua hover:bg-merah-gelap text-putih font-medium py-2 px-4 rounded-lg">
+                                            <a href="#" data-toggle="modal" data-target="#ModalShow{{ $data->id_kk }}" class="detail bg-abu-tua hover:bg-merah-gelap text-putih font-medium py-2 px-4 rounded-lg">
                                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
                                                     <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
                                                     <path fill-rule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z" clip-rule="evenodd" />
                                                 </svg>
                                             </a>
-                                            {{-- <form action="{{ url('/RW/StrukturRw/' . $data->id_struktur) }}" method="POST">
+                                            <form action="{{ url('/RW/KartuKeluarga/' . $data->id_kk) }}" method="POST">
                                                 @csrf
                                                 @method('DELETE')
                                                 <button type="submit" class="bg-merah hover:bg-merah-gelap text-putih font-medium py-2 px-4 rounded-lg" onclick="return confirm('Apakah anda ingin menghapus data ini ?')">
@@ -105,7 +110,7 @@
                                                         <path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clip-rule="evenodd" />
                                                     </svg>
                                                 </button>
-                                            </form> --}}
+                                            </form>
                                         </div>
                                     </td>
                                 </tr>
@@ -130,7 +135,7 @@
 
 </body>
 </html>
-@include('RW.StrukturRw.show')
+@include('RW.KartuKeluarga.show')
 
 
 @endsection
