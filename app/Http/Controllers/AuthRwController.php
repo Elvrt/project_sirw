0163f8cf11bb782740ae50eb973c2f26b07d66df
Nafi'ul Alam Dary Vega <120392921+NafiulAlamDaryVega@users.noreply.github.com>
2024-06-02 23:27:45 +0700

    edit dashboard rw

--- app/Http/Controllers/AuthRwController.php
+++ app/Http/Controllers/AuthRwController.php
@@ -21,6 +21,19 @@ class AuthRwController extends Controller
         $lakiCount = WargaModel::where('jenis_kelamin', 'L')->count();
         $perempuanCount = WargaModel::where('jenis_kelamin', 'P')->count();
 
+        $sumIuran = IuranModel::whereYear('tanggal_iuran', 2024)->sum('nominal');
+        $sumIuranJan = IuranModel::whereYear('tanggal_iuran', 2024)->whereMonth('tanggal_iuran', 1)->sum('nominal');
+        $sumIuranFeb = IuranModel::whereYear('tanggal_iuran', 2024)->whereMonth('tanggal_iuran', 2)->sum('nominal');
+        $sumIuranMar = IuranModel::whereYear('tanggal_iuran', 2024)->whereMonth('tanggal_iuran', 3)->sum('nominal');
+        $sumIuranApr = IuranModel::whereYear('tanggal_iuran', 2024)->whereMonth('tanggal_iuran', 4)->sum('nominal');
+        $sumIuranMay = IuranModel::whereYear('tanggal_iuran', 2024)->whereMonth('tanggal_iuran', 5)->sum('nominal');
+        $sumIuranJun = IuranModel::whereYear('tanggal_iuran', 2024)->whereMonth('tanggal_iuran', 6)->sum('nominal');
+        $sumIuranJul = IuranModel::whereYear('tanggal_iuran', 2024)->whereMonth('tanggal_iuran', 7)->sum('nominal');
+        $sumIuranAug = IuranModel::whereYear('tanggal_iuran', 2024)->whereMonth('tanggal_iuran', 8)->sum('nominal');
+        $sumIuranSep = IuranModel::whereYear('tanggal_iuran', 2024)->whereMonth('tanggal_iuran', 9)->sum('nominal');
+        $sumIuranOct = IuranModel::whereYear('tanggal_iuran', 2024)->whereMonth('tanggal_iuran', 10)->sum('nominal');
+        $sumIuranNov = IuranModel::whereYear('tanggal_iuran', 2024)->whereMonth('tanggal_iuran', 11)->sum('nominal');
+        $sumIuranDec = IuranModel::whereYear('tanggal_iuran', 2024)->whereMonth('tanggal_iuran', 12)->sum('nominal');
         return view(
             'RW.index',
             [
@@ -30,6 +43,19 @@ class AuthRwController extends Controller
                 'pengaduanCount' => $pengaduanCount,
                 'lakiCount' => $lakiCount,
                 'perempuanCount' => $perempuanCount,
+                'sumIuran' => $sumIuran,
+                'sumIuranJan' => $sumIuranJan,
+                'sumIuranFeb' => $sumIuranFeb,
+                'sumIuranMar' => $sumIuranMar,
+                'sumIuranApr' => $sumIuranApr,
+                'sumIuranMay' => $sumIuranMay,
+                'sumIuranJun' => $sumIuranJun,
+                'sumIuranJul' => $sumIuranJul,
+                'sumIuranAug' => $sumIuranAug,
+                'sumIuranSep' => $sumIuranSep,
+                'sumIuranOct' => $sumIuranOct,
+                'sumIuranNov' => $sumIuranNov,
+                'sumIuranDec' => $sumIuranDec,
             ]
         );
     }
